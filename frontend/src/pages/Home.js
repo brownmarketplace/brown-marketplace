10737e827bba87e6cce7a798053191a0c447376c
Mike-Do <agentdonoso@gmail.com>
2022-05-03 21:49:37 -0400

    feat Add home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,25 @@
-const Home = () => {
-    return (
-        <div className="home">
-            <h1>Home</h1>
+import React from 'react'
+import BoilerplateHeader from '../components/BoilerplateHeader'
+import Footer from '../components/Footer'
+import defaultProfilePicture from '../images/pfp.png'
+
+import './boilerplate-page.css'
+
+function Home(props) {
+  return (
+    <div className="boilerplate">
+        <BoilerplateHeader title={props.title} userPicture={props.pfp}/>
+        <div style={{ textAlign: 'center', marginTop: '30px' }}>
+            Put your content here.
         </div>
-    );
+        {/* <Footer/> */}
+    </div>
+  )
 }
 
-export default Home;
+Home.defaultProps = {
+  title: "Brown Marketplace",
+  pfp: defaultProfilePicture
+}
+
+export default Home
