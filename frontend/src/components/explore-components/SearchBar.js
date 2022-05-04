a511560da0ec25dc70e1fb888876714a13a3a73c
Mike-Do <agentdonoso@gmail.com>
2022-05-04 19:48:22 -0400

    feat Add searching functionality

--- frontend/src/components/explore-components/SearchBar.js
+++ frontend/src/components/explore-components/SearchBar.js
@@ -12,24 +12,35 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar() {
+    const [input, setInput] = useState('');
     const [isFocused, toggleIsFocused] = useState(false)
     const searchRef = useRef(null)
+
+    // Search function that routes to the search results
+    const search = e => {
+        e.preventDefault();
+        // Route to the category search page for the input "category/input"
+        window.location.href = `/category/${input}`
+    }
+
 
     return (
         <div className="search-bar-container">
             <div className={`search-bar ${isFocused ? "on-focus" : "no-focus"}`} ref={searchRef}>
                 <Paper elevation={isFocused ? 3 : 0}>
-                    <div className="search-icon">
-                        <IconButton>
-                            <SearchIcon/>
+                    {/* Used a form and made IconButton type submit to detect clicking enter */}
+                    <form className="search-icon">
+                        <IconButton type="submit" onClick={search}>
+                            <SearchIcon className="search-icon"/>
                         </IconButton>
                         <input
-                            onChange={e => console.log(e.currentTarget.value)}
+                            value={input}
+                            onChange={e => setInput(e.target.value)}
                             onFocus={() => toggleIsFocused(true)}
                             onBlur={() => setTimeout(() => toggleIsFocused(false), 500)}
                             className="search-input"
                             placeholder="Search"/>
-                    </div>
+                    </form>
                 </Paper>
             </div>
         </div>
