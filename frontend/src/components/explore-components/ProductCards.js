f645f2bb2fac14e7387f20a4585f4f693782b4fd
Mike-Do <agentdonoso@gmail.com>
2022-05-04 20:02:43 -0400

    feat Add new sample data

--- frontend/src/components/explore-components/ProductCards.js
+++ frontend/src/components/explore-components/ProductCards.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"; 
+import React, { useState, useMemo, useRef } from 'react'
 
 // Component Imports
 import TinderCard from "react-tinder-card";
@@ -9,75 +9,98 @@ import './product-cards.css'
 function ProductCards() {
     const [products, setProducts] = useState([
         {
-            name: 'Mushroom Crochets',
-            url: 'https://embed.filekitcdn.com/e/pZVtAQBFhqs4ADp3yRAnVv/6J9S2S2ez1N2JNCkbxCfKt'
+            name: 'Frog',
+            url: 'https://www.aquariumofpacific.org/images/exhibits/Magnificent_Tree_Frog_900.jpg'
+        },
+        {
+            name: 'Flamingo',
+            url: 'https://i.pinimg.com/564x/e6/33/34/e63334958361afd77c65e8cdf0ad62ac.jpg'
+        },
+        {
+            name: 'Fox',
+            url: 'https://i.pinimg.com/564x/ef/49/37/ef493790714a037449d62d3f2a6fccbf.jpg'
+        },
+        {
+            name: 'Seal',
+            url: 'https://i.pinimg.com/564x/0e/81/6e/0e816e21de2f3ed8f12b3b8426a35bac.jpg'
+        },
+        {
+            name: 'Meerkat',
+            url: 'https://i.pinimg.com/564x/94/d4/ac/94d4acf4890271614be7018e7f035efe.jpg'
         },
         {
             name: 'Mushroom Crochets',
             url: 'https://embed.filekitcdn.com/e/pZVtAQBFhqs4ADp3yRAnVv/6J9S2S2ez1N2JNCkbxCfKt'
         },
-        {
-            name: 'Mushroom Crochets',
-            url: 'https://embed.filekitcdn.com/e/pZVtAQBFhqs4ADp3yRAnVv/6J9S2S2ez1N2JNCkbxCfKt'
-        },
-        {
-            name: 'Mushroom Crochets',
-            url: 'https://embed.filekitcdn.com/e/pZVtAQBFhqs4ADp3yRAnVv/6J9S2S2ez1N2JNCkbxCfKt'
-        },
-        {
-            name: 'Mushroom Crochets',
-            url: 'https://embed.filekitcdn.com/e/pZVtAQBFhqs4ADp3yRAnVv/6J9S2S2ez1N2JNCkbxCfKt'
-        }
         ]);
 
 
-    // create a list of example data for the cards
-    const characters = [
-    {
-        name: 'Mushroom Crochets',
-        url: 'https://embed.filekitcdn.com/e/pZVtAQBFhqs4ADp3yRAnVv/6J9S2S2ez1N2JNCkbxCfKt'
-    },
-    {
-        name: 'Mushroom Crochets',
-        url: 'https://embed.filekitcdn.com/e/pZVtAQBFhqs4ADp3yRAnVv/6J9S2S2ez1N2JNCkbxCfKt'
-    },
-    {
-        name: 'Mushroom Crochets',
-        url: 'https://embed.filekitcdn.com/e/pZVtAQBFhqs4ADp3yRAnVv/6J9S2S2ez1N2JNCkbxCfKt'
-    },
-    {
-        name: 'Mushroom Crochets',
-        url: 'https://embed.filekitcdn.com/e/pZVtAQBFhqs4ADp3yRAnVv/6J9S2S2ez1N2JNCkbxCfKt'
-    },
-    {
-        name: 'Mushroom Crochets',
-        url: 'https://embed.filekitcdn.com/e/pZVtAQBFhqs4ADp3yRAnVv/6J9S2S2ez1N2JNCkbxCfKt'
-    }
-    ]
 
-    // create state for the cards and the direction they are swiped in
+    
+
+    const [currentIndex, setCurrentIndex] = useState(products.length - 1)
     const [lastDirection, setLastDirection] = useState()
+    // used for outOfFrame closure
+    const currentIndexRef = useRef(currentIndex)
 
-    const [drawerOpen, toggleDrawer] = useState(false)
+    const childRefs = useMemo(
+        () =>
+        Array(products.length)
+            .fill(0)
+            .map((i) => React.createRef()),
+        []
+    )
 
-    // create state for the cards and the direction they are swiped in
-    const swiped = (direction, nameToDelete) => {
-        setLastDirection(direction)
+    const updateCurrentIndex = (val) => {
+        setCurrentIndex(val)
+        currentIndexRef.current = val
     }
 
-    // create state for the cards and the direction they are swiped in
-    const outOfFrame = (name) => {
-        console.log(name + ' left the screen!')
-        // TODO: this is for action when a card is swiped out of the screen
+    const canGoBack = currentIndex < products.length - 1
+
+    const canSwipe = currentIndex >= 0
+
+    // set last direction and decrease current index
+    const swiped = (direction, nameToDelete, index) => {
+        setLastDirection(direction)
+        updateCurrentIndex(index - 1)
+    }
+
+    const outOfFrame = (name, idx) => {
+        console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current)
+        // handle the case in which go back is pressed before card goes outOfFrame
+        currentIndexRef.current >= idx && childRefs[idx].current.restoreCard()
+        // TODO: when quickly swipe and restore multiple times the same card,
+        // it happens multiple outOfFrame events are queued and the card disappear
+        // during latest swipes. Only the last outOfFrame event should be considered valid
+    }
+
+    const swipe = async (dir) => {
+        if (canSwipe && currentIndex < products.length) {
+        await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
+        }
+    }
+
+    // increase current index and show card
+    const goBack = async () => {
+        if (!canGoBack) return
+        const newIndex = currentIndex + 1
+        updateCurrentIndex(newIndex)
+        await childRefs[newIndex].current.restoreCard()
     }
 
     return (
         <div className="cards-container">
-            {products.map(product => (
+            {products.map((product, index) => (
                 <TinderCard
+                    ref={childRefs[index]}
                     className="swipe"
                     // give each card a unique key, efficient to re-render
                     key={product.name}
+                    // update current index when card is swiped
+                    onSwipe={(dir) => swiped(dir, product.name, index)}
+                    // when card leaves screen, update current index
+                    onCardLeftScreen={() => outOfFrame(product.name, index)}
                     // only allow swiping left or right
                     preventSwipe={['up', 'down']}
                 >
