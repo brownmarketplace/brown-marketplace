c4c942fbf48ca250460ca7837b6abe7d900f3b98
Sophia Liu <sophia_liu@brown.edu>
2022-05-13 16:48:35 -0400

    changed website header from text to logo

--- frontend/src/components/boilerplate-components/Title.js
+++ frontend/src/components/boilerplate-components/Title.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo from "../../images/logo.png"
 import './title.css'
 
 function Title(props) {
   return (
     <div className="boilerplate-title">
-      <div>{props.title}</div>
+      <img src={logo}></img>
     </div>
   )
 }
