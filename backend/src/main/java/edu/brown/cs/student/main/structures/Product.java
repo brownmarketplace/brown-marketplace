7310283ff2120b77d533e956b8a41b1450d32a6f
Carmen Yu <carmen_yu@brown.edu>
2022-05-13 15:06:27 -0400

    bug: errors when running backend

--- backend/src/main/java/edu/brown/cs/student/main/structures/Product.java
+++ backend/src/main/java/edu/brown/cs/student/main/structures/Product.java
@@ -9,25 +9,28 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * This class models a Product. Although a Product is instantiated from the fields relevant to the
- * recommender system (our primary backend functionality), the rest of the fields are still
- * maintained with getters and setters in the case of extended functionality in the future.
+ * This class models a Product.
  */
 public class Product implements BFInsertable {
   private Map<String, Object> attributes;
   private BloomFilter bf;
-//  private double xnorSimilarity;
+  private double xnorSimilarity;
 
   /**
-   * The constructor of Product. Since the overall dataset isn't being stored and consistently
-   * updated in the back end, a Product is instantiated using only the fields relevant to the
-   * recommender system.
+   * The constructor of Product.
+   *
+   * @param attributes a Map with attribute names as the keys and the corresponding attributes as
+   *                   values
    */
   public Product(Map<String, Object> attributes) {
     this.attributes = attributes;
   }
 
-
+  /**
+   * This is the getter for the product ID.
+   *
+   * @return product ID
+   */
   @Override
   public String getId() {
     return (String) (this.attributes.get("id"));
@@ -100,12 +103,22 @@ public class Product implements BFInsertable {
     return filter;
   }
 
+  /**
+   * This is the getter for the product's bloom filter.
+   *
+   * @return bloom filter of the product
+   */
   @Override
   public BloomFilter getBf() {
     return this.bf;
   }
 
-//  public void setXNORSimilarity(double similarity) {
-//    this.xnorSimilarity = (double) similarity;
-//  }
+  /**
+   * This is the setter for the XNOR similarity of the product.
+   *
+   * @param similarity XNOR similarity of the product
+   */
+  public void setXNORSimilarity(double similarity) {
+    this.xnorSimilarity = (double) similarity;
+  }
 }
