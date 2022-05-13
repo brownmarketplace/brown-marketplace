eb8c9f6e527713d6982cc611091695f566110b27
TianranZhang <tianran_zhang@brown.edu>
2022-05-12 21:25:22 -0400

    recommend

--- frontend/src/Recommend-api.js
+++ frontend/src/Recommend-api.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 // Load the data from backend
@@ -8,16 +8,20 @@ function RecommendApi() {
 
     const postConfig = {headers: {}}
 
+    useEffect(() => {
+        let toSend = {user: "u1"}
+        const recommendUrl = "http://127.0.0.1:4567/recommend"
+        axios.post(recommendUrl, toSend, postConfig)
+            .then((response) => {
+                console.log("recommendation loaded successfully");
+                setExplore(response.data['result']);
+                console.log(response.data)
+            })
+            .catch(e => console.log("Erroring"))
+        }, []);
+
     // Send the user id to backend
-    let toSend = {user: "u1"}
-    const recommendUrl = "http://127.0.0.1:4567/recommend"
-    axios.post(recommendUrl, toSend, postConfig)
-        .then((response) => {
-            console.log("recommendation loaded successfully");
-            setExplore(response.data['result']);
-            console.log(response.data)
-        })
-        .catch(e => console.log("Erroring"))
+    
     
     return (
         <div>
