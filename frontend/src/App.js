43458635606c22711a4a22f96d7bc26fb04b0e6a
TianranZhang <tianran_zhang@brown.edu>
2022-07-13 18:54:44 -0400

    feat: chat in progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import ProfilePage from './pages/ProfilePage';
 import AddListing from './pages/AddListing';
 import ProductPage from './pages/ProductPage';
 import CategoryPage from './pages/CategoryPage';
+import Chat from './pages/ChatCentral';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Cookies from 'universal-cookie';
@@ -13,6 +14,7 @@ import axios from 'axios';
 import { ref, set, get, onValue, query, orderByChild, equalTo, child }
   from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
 import database from './backend/Database/DBInstance'
+import ChatCentral from './pages/ChatCentral';
 
 const App = () => {
 
@@ -64,9 +66,10 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<BoilerplatePage userID={cookies.get("userID")} />} />
+        <Route path="/chat" element={<ChatCentral/>} />
         <Route path="/explore" element={<Explore userID={cookies.get("userID")} loginState={loginState} logoutState={logoutState} />} />
         <Route path="/profile" element={<ProfilePage pfp={cookies.get("pfp")} name={cookies.get("name")} email={cookies.get("email")} userID={cookies.get("userID")} />}>
-          <Route path=":userid" element={<ProfilePage pfp={cookies.get("pfp")} name={cookies.get("name")} email={cookies.get("email")} userID={cookies.get("userID")} />} />
+        <Route path=":userid" element={<ProfilePage pfp={cookies.get("pfp")} name={cookies.get("name")} email={cookies.get("email")} userID={cookies.get("userID")} />} />
         </Route>
         <Route path="/sell" element={<AddListing userID={cookies.get("userID")} />} />
         <Route path="/product/:productId" element={<ProductPage userID={cookies.get("userID")} />} />
