9079ba7a8a5e266dc73e8d17eeb0bda2e5b68148
Mike-Do <agentdonoso@gmail.com>
2022-08-20 18:19:15 -0400

    style Remove old styles

--- frontend/src/pages/Explore.js
+++ frontend/src/pages/Explore.js
@@ -1,29 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 // Component Imports
-import BoilerplateHeader from '../components/BoilerplateHeader'
 import ExploreHeader from '../components/explore-components/ExploreHeader'
-import Footer from '../components/Footer'
-import SearchBar from '../components/explore-components/SearchBar'
-import Title from '../components/boilerplate-components/Title'
-import TinderCard from 'react-tinder-card'
 import ProductCardsV2 from '../components/explore-components/ProductCardsV2'
-import Advanced from '../components/explore-components/ProductCardsV2'
-import ProductButtons from '../components/explore-components/ProductButtons'
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
+
 
 // CSS Imports
-import './boilerplate-page.css'
 import './explore.css'
-import '../components/explore-components/advanced.css'
+// import '../components/explore-components/advanced.css'
 
 // Image Icon Imports
 import defaultProfilePicture from '../images/pfp.png'
 
 function Explore(props) {
   return (
-    <div className="cards">
+    <div>
       <ProductCardsV2 userID={props.userID} isLoggedIn={props.loginState} />
     </div>
   )
